f17264f331338daee2a0f5fabb409b446379386e
Ulanov Ivan <veloteh@outlook.com>
2022-01-15 20:55:17 +0300

    DriverClass

---
+++ src/main/java/com/epam/mobile/testing/driver/DriverManager.java
@@ -0,0 +1,63 @@
+package com.epam.mobile.testing.driver;
+
+import com.epam.mobile.testing.configuration.ConfigurationReader;
+import com.epam.mobile.testing.configuration.EnvironmentType;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import static java.lang.String.format;
+
+public class DriverManager {
+
+    private static final Logger LOG = LogManager.getRootLogger();
+    private static final EnvironmentType ENVIRONMENT_TYPE =
+            EnvironmentType.valueOf(ConfigurationReader.get().env().toUpperCase());
+    private static AppiumDriver<MobileElement> driver;
+
+    private DriverManager() {
+
+    }
+
+    public static AppiumDriver<MobileElement> getDriver() {
+        if (driver == null) {
+            driver = createDriver();
+        }
+        return driver;
+    }
+
+    private static AppiumDriver<MobileElement> createDriver() {
+        switch (ENVIRONMENT_TYPE) {
+            case LOCAL:
+                driver = new AndroidDriver<>(null);
+                break;
+            default:
+                throw new IllegalArgumentException(format("Unexpected environment value: %s", ENVIRONMENT_TYPE));
+        }
+        LOG.info("Driver is created");
+        LOG.info("Environment type is {}", ENVIRONMENT_TYPE);
+        return driver;
+    }
+
+    public static void closeDriver() {
+        Optional.ofNullable(getDriver()).ifPresent(driverInstance -> {
+            driverInstance.quit();
+            driver = null;
+            LOG.info("Driver is closed");
+        });
+    }
+
+    public static void closeEmulator() {
+        try {
+            Runtime.getRuntime().exec(format("adb -s %s emu kill", ConfigurationReader.get().udid()));
+            LOG.info("AVD is closed");
+        } catch (IOException e) {
+            LOG.info("AVD was not closed, message: {}", e.getMessage());
+        }
+    }
+}
---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,23 @@
+import com.epam.mobile.testing.driver.DriverManager;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+
+public class BaseTest {
+
+    @BeforeClass
+    public void createSession() {
+        DriverManager.getDriver();
+    }
+
+    @AfterMethod
+    public void resetApp() {
+        DriverManager.getDriver().resetApp();
+    }
+
+    @AfterClass
+    public void closeSession() {
+        DriverManager.closeDriver();
+        DriverManager.closeEmulator();
+    }
+}
